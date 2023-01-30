cf8f82802bb21ed7de5bd4188546885f61c7a8bc
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-30 17:42:43 -0500

    feat: Add basic description

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,14 @@ import './home.scss';
 export const Home = () => {
   return (
     <header className='home'>
+        <div className='home__content'>
+
+          <p className='content-intro'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis atque ipsum, quisquam accusantium voluptas placeat in quas maiores error cum ipsam explicabo dicta laborum. Reiciendis nam sunt optio totam quibusdam?</p>
+          <button className='content-btn'>
+            Started
+          </button>
+
+        </div>
 
         <svg className='home-waves' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,192L60,165.3C120,139,240,85,360,64C480,43,600,53,720,69.3C840,85,960,107,1080,122.7C1200,139,1320,149,1380,154.7L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
     </header>
