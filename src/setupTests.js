f648c9931c30bdbe714f92243efe7e17b0a3c210
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-31 18:54:50 -0500

    feat: add testing library setting to use id like test-id

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
 
+configure({testIdAttribute: 'id'})
 
 window.matchMedia = window.matchMedia || function() {
     return {
