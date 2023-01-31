45d5df5a626b48ccab0a63cec2a0ce57b0f0e882
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-31 00:30:38 -0500

    feat: show components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Home, Navbar, Services } from "./components";
+import { Footer, Gallery, Home, Navbar, Services } from "./components";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <main className="App">
       <Navbar />
       <Home />
+      <Gallery />
       <Services />
+      <Footer />
     </main>
   );
 }
