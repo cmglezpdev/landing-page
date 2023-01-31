032a2f05c199d230c45b629c2600fae4e128c7ea
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-31 14:23:36 -0500

    feat: Delete some cars

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -68,39 +68,15 @@ export const Gallery = () => {
                 />
 
                 <CardCar
-                    img='assets/gallery/ferrari.jpg'
-                    name='Ferrari'  
-                    info='Ferrari es una de las firmas de automóviles más conocidas a nivel mundial. Especializada en la fabricación de súper-deportivos de altas prestaciones, además han logrado a lo largo de su historia multitud de éxitos en competición.'
-                />
-
-                <CardCar
                     img='assets/gallery/tesla.jpg'
                     name='Tesla'
                     info='Tesla es una marca de automóviles de lujo estadounidense, perteneciente al grupo Tesla. La compañía fue fundada en 2003 por Elon Musk, quien también diseñó el primer modelo de la marca.'
                 />
 
                 <CardCar
-                    img='assets/gallery/toyota.jpg'
-                    name='Toyota'
-                    info='Toyota es una marca de automóviles de lujo japonesa, perteneciente al grupo Toyota. La compañía fue fundada en 1937 por Kiichiro Toyoda, quien también diseñó el primer modelo de la marca.'
-                />
-
-                <CardCar
                     img='assets/gallery/nissan.jpg'
                     name='Nissan'
                     info='Nissan es una marca de automóviles de lujo japonesa, perteneciente al grupo Nissan. La compañía fue fundada en 1933 por Yoshisuke Aikawa, quien también diseñó el primer modelo de la marca.'
-                />
-
-                <CardCar
-                    img='assets/gallery/mazda.jpg'
-                    name='Mazda'
-                    info='Mazda es una marca de automóviles de lujo japonesa, perteneciente al grupo Mazda. La compañía fue fundada en 1920 por Jujiro Matsuda, quien también diseñó el primer modelo de la marca.'
-                />
-
-                <CardCar
-                    img='assets/gallery/honda.jpg'
-                    name='Honda'
-                    info='Honda es una marca de automóviles de lujo japonesa, perteneciente al grupo Honda. La compañía fue fundada en 1948 por Soichiro Honda, quien también diseñó el primer modelo de la marca.'
                 />
 
                 <CardCar
