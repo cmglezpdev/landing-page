bd6355490400488b29a6d3a54c260b459fe8764d
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-31 00:29:59 -0500

    feat: Add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import { IoCarSportSharp } from 'react-icons/io5';
+import { BsInstagram, BsTwitter } from 'react-icons/bs';
+
+import './footer.scss';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__info">
+                <IoCarSportSharp className="info__logo" />
+
+                <div className='info__legal'>
+                    <span className='terms'>Termino y Condiciones</span>
+                </div>
+                <div className="info__sn">
+                    <BsInstagram className='sn-icon' />
+                    <BsTwitter className='sn-icon' />
+                </div>
+            </div>
+        </footer>
+    )
+}
